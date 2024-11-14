121c1b5a9b3c80a197ab2050cc2ace31f0f4fa81
victor-molinero <30585418+victor-molinero@users.noreply.github.com>
2024-11-14 13:25:09 -0500

    task 3 completed

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,5 @@
 '''module responsible for querying the Watson NLP API'''
-import requests
+import requests, json
 
 urlbase = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService'
 
@@ -8,5 +8,18 @@ def emotion_detector(text_to_analyse):
     endpoint = '/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyse } }
-    x = requests.post(url = urlbase + endpoint, json = input_json, headers = headers)
-    return x.text
+    response = requests.post(url = urlbase + endpoint, json = input_json, headers = headers)
+    emotionsList = json.loads(response.text)['emotionPredictions'][0]['emotion']
+
+    dominant_emotion = ''
+    dominant_value = 0
+    result = {}
+
+    for em_key in emotionsList:        
+        result[em_key] = emotionsList[em_key]
+        if emotionsList[em_key] >= dominant_value:
+            dominant_emotion = em_key
+            dominant_value = emotionsList[em_key]
+    result["dominant_emotion"] = dominant_emotion 
+
+    return result
